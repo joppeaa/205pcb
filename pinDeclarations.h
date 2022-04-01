81aa956dd96f06d800dbeefeff65453205aa8a42
Joppe Aarnoutse <joppeaa@gmail.com>
2022-04-01 23:36:57 +0200

    Added some pinDeclarations

--- pinDeclarations.h
+++ pinDeclarations.h
@@ -1,1 +1,13 @@
+#define mainrelayPin 30
+#define hornPin 5
+#define mirrorheatPin 10
+#define cabinPin 35
+#define indicatorPin 36
+#define headlightPin 6
+#define heaterPin 9
+#define servovoltagePin 25
 
+#define 433A 14                       //433 Module inputs
+#define 433B 15
+#define 433C 16
+#define 433D 17
