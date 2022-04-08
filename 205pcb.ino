03b9ba7fd81ee8ec4ddee1ae8247559d7677d095
Joppe Aarnoutse <joppeaa@gmail.com>
2022-04-08 23:48:35 +0200

    Added some flags

--- 205pcb.ino
+++ 205pcb.ino
@@ -5,8 +5,8 @@ const unsigned int maindelayMax = 50;
 bool checkinputsFlag = 0;
 
 
-const int SHORT_PRESS_TIME = 1000; // 1000 milliseconds
-const int LONG_PRESS_TIME  = 1000; // 1000 milliseconds
+const int SHORT_PRESS_TIME = 1500; // 1000 milliseconds
+const int LONG_PRESS_TIME  = 1500; // 1000 milliseconds
 
 int currentState433A;
 int lastState433A = LOW;
@@ -15,6 +15,16 @@ unsigned long releasedTime433A = 0;
 bool isPressing433A = false;
 bool isLongDetected433A = false;
 
+
+
+bool remoteshortpressFlagA = false;
+bool remotelongpressFlagA = false;
+bool remoteshortpressFlagB = false;
+bool remotelongpressFlagB = false;
+bool remoteshortpressFlagC = false;
+bool remotelongpressFlagC = false;
+bool remoteshortpressFlagD = false;
+bool remotelongpressFlagD = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -49,20 +59,21 @@ if (maindelay > maindelayMax)               //Main high speed loop
 void checkremoteInputs()
 {
   currentState433A = digitalRead(remotePinA);
-  if(lastState433A == LOW && currentState433A == HIGH)         // button is pressed
+  if(lastState433A == LOW && currentState433A == HIGH)         //button is pressed
   {
     pressedTime433A = millis();
     isPressing433A = true;
     isLongDetected433A = false;
   }
-  else if(lastState433A == HIGH && currentState433A == LOW)     // button is released
+  else if(lastState433A == HIGH && currentState433A == LOW)     //button is released
   { 
     isPressing433A = false;
     releasedTime433A = millis();
     long pressDuration433A = releasedTime433A - pressedTime433A;
     if(pressDuration433A < SHORT_PRESS_TIME)
     {
-      Serial.println("A short press is detected");
+      Serial.println("A short press is detected");              //Short press detected
+      bool remoteshortpressFlagA = true;
       flashLED(3);
     }
   }
@@ -71,8 +82,10 @@ void checkremoteInputs()
     long pressDuration433A = millis() - pressedTime433A;
     if( pressDuration433A > LONG_PRESS_TIME ) 
     {
-      Serial.println("A long press is detected");
+      Serial.println("A long press is detected");               //Long press detected
+      
       isLongDetected433A = true;
+      remotelongpressFlagA = true;
       flashLED(10);
     }
   }
